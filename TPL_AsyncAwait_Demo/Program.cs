41c958c4979a72f90d9b8ab3ee282f4e860d4681
James Walsh Jr <walshjrjames@outlook.com>
2018-10-21 20:04:25 -0400

    Initial Check In

--- TPL_AsyncAwait_Demo/Program.cs
+++ TPL_AsyncAwait_Demo/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+
 
 namespace TPL_AsyncAwait_Demo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            try
+            {
+                GetToDo todoRequest = new GetToDo();
+
+                var response =  await todoRequest.GetToDoAsynchronous();
+                Console.WriteLine(response);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
+           
         }
+    }
+
+    public class GetToDo
+    {
+        public readonly HttpClient client;
+
+        public GetToDo()
+        {
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(Consts.BaseUrl)
+            };
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(Consts.MediaType));
+        }
+       
+
+        public async Task<string> GetToDoAsynchronous()
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(Consts.ToDo);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return responseContent;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+                throw;
+            }
+            
+        }
+    }
+
+     public static class Consts
+    {
+        public const string BaseUrl = "https://jsonplaceholder.typicode.com";
+        public const string ToDo = "/todos";
+        public const string MediaType = "application/json";
     }
 }
